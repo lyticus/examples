817a2805aa72312fa9081bc11227741c4386c532
Bjorn Krols <krols.bj@gmail.com>
2019-05-29 13:58:41 +0200

    Universal mode

---
+++ nuxt/universal-mode/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maiores
+      incidunt aliquid sequi expedita qui, nostrum eum minus, harum inventore
+      blanditiis optio sit tempora atque voluptas quos. Ad, itaque voluptas.
+    </p>
+    <button @click="$lyticus.trackClick('yellow-button')" style="background: yellow">Yellow button</button>
+  </div>
+</template>
